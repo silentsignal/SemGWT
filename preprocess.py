9ef643bdbf210a6525f2edfcd80d0f25914cee4a
b <vpbalint@silentsignal.hu>
2022-03-13 09:35:25 +0100

    rebase for release

---
+++ preprocess.py
@@ -0,0 +1,26 @@
+import os
+import re
+import sys
+import ast
+
+RE_ONSCRIPTDOWNLOADED=re.compile("^[^\\.]+\\.onScriptDownloaded\\(")
+RE_BODYSCRIPT=re.compile("<body><script><!--")
+cache=open(sys.argv[1],"r").read()
+dirname=os.path.basename(sys.argv[1]).split('.')[0]
+
+if "onScriptDownloaded" in cache: # version X
+    os.mkdir(dirname)
+    raw_arr=RE_ONSCRIPTDOWNLOADED.sub("",cache)[:-3]
+    arr=ast.literal_eval(raw_arr) # this can result in code exec but json doesn't like single quites :P
+    for n,v in enumerate(arr):
+        with open(os.path.join(dirname,"%d.js" % (n)),"w") as out:
+            out.write(v)
+elif cache.startswith("<html>"): # version Y
+    os.mkdir(dirname)
+    parts=cache.split("<script><!--")
+    for n,v in enumerate(parts):
+        if n == 0:
+            continue
+        with open(os.path.join(dirname,"%d.js" % (n)),"w") as out:
+            out.write(v.replace("--></script>","").replace("</body></htlm>",""))
+    
